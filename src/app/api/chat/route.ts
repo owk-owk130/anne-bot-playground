0e837d1eba1b34bb50126e9c19dee3eda487cb07
owk-owk130 <owaki-kengo@kayac.com>
2025-06-02 17:08:24 +0900

    remove: console

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,7 +3,6 @@ import { mastra } from "~/mastra";
 export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
-    console.log("Received messages:", messages);
     const myAgent = mastra.getAgent("catAgent");
     const stream = await myAgent.stream(messages);
     return stream.toDataStreamResponse();
