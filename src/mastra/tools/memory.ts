307038d6377262be7c20dedf3a5038726cd0c0d6
owk-owk130 <owaki-kengo@kayac.com>
2025-06-04 12:53:00 +0900

    fix: 環境変数名が間違っていた

--- src/mastra/tools/memory.ts
+++ src/mastra/tools/memory.ts
@@ -1,11 +1,11 @@
 import { Memory } from "@mastra/memory";
 import { PgVector, PostgresStore } from "@mastra/pg";
 
-const host = process.env.POSTGRES_HOST || "";
-const port = (process.env.POSTGRES_PORT || 0) as number;
-const user = process.env.POSTGRES_USER || "";
-const database = process.env.POSTGRES_DB_NAME || "";
-const password = process.env.POSTGRES_PASSWORD || "";
+const host = process.env.SUPABASE_DB_HOST || "";
+const port = (process.env.SUPABASE_DB_PORT || 0) as number;
+const user = process.env.SUPABASE_DB_USER || "";
+const database = process.env.SUPABASE_DB_NAME || "";
+const password = process.env.SUPABASE_DB_PASSWORD || "";
 const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`;
 
 export const memory = new Memory({
